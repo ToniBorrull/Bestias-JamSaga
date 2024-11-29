e128155072d2b6596c21368b876d4e30b2282c84
ToniBorrull <146344011+ToniBorrull@users.noreply.github.com>
2024-11-29 18:28:54 +0100

    Drake

--- Assets/Scripts/Enemies/Boss1/Boss1.cs
+++ Assets/Scripts/Enemies/Boss1/Boss1.cs
@@ -9,6 +9,8 @@ public class Boss1 : Enemy
     public Transform AttackSlot1;
     public Transform AttackSlot2;
     private Animator animator;
+    private float lastAttack = 0f;
+    public float waitTime;
 
     protected override void Start()
     {
@@ -17,25 +19,57 @@ public class Boss1 : Enemy
 
     protected override void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Time.time > lastAttack + atkRate)
         {
-            Combo1();
+            waitTime = Random.Range(1, 3);
+            ChooseAttack();
         }
     }
     protected override void Die()
     {
-
     }
+    void ChooseAttack()
+    {
+        //3 ataques de 0 a 2
+        float chosenAttack = Mathf.Floor(Random.Range(0, 3));
 
+        //Debug.Log("ChosenAttack: " + chosenAttack);
+       
+        switch (chosenAttack)
+        {
+            case 0:
+                Combo1();
+            break;
+                case 1:
+                    Combo2();
+                break;
+                    case 2:
+                        Combo3();
+                    break;
+            default:
+            break;
+                    
+        }
+    }
     void Combo1()
     {
-        StartCoroutine(ControllerCombo());
+        StartCoroutine(ControllerCombo(waitTime));
     }
-    IEnumerator ControllerCombo()
+    void Combo2() {
+       // StartCoroutine();
+    }
+    void Combo3() {
+    }
+
+    IEnumerator ControllerCombo(float waitTime)
     {
+        lastAttack = Time.time + waitTime;
         animator.SetBool("HighPos", true);
         Instantiate(projectile, AttackSlot1.position, transform.rotation);
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(waitTime);
+
+        //Aqui se puede añadir un wait for animator clip (en el futuro)
+        
         animator.SetBool("HighPos", false);
         Instantiate(projectile, AttackSlot2.position, transform.rotation);
     }
