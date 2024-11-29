df41950b65e3de3b60fc19b28c2c428f7f36cbc9
AleixAznarENTI <aleix.aznar@enti.cat>
2024-11-29 17:00:58 +0100

    Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float health;
+    public float atkRate;
+    protected abstract void Start();
 
-    // Update is called once per frame
-    void Update()
+    protected abstract void Update();
+
+    public virtual void TakeDamage(float dmg)
     {
-        
+        health -= dmg;
+        health = health <= 0 ? 0 : health;
+        if (health < 0) Die();
     }
+    protected abstract void Die();
 }
