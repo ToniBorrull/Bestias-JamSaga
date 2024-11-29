39ee529b8273b2943a5246c570d96f3080f73cd9
ToniBorrull <146344011+ToniBorrull@users.noreply.github.com>
2024-11-29 17:08:32 +0100

    GM

---
+++ Assets/Scripts/Boss1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1 : Enemy
+{
+
+    protected override void Start()
+    {
+
+    }
+
+    protected override void Update()
+    {
+
+    }
+    protected override void Die()
+    {
+
+    }
+
+}
