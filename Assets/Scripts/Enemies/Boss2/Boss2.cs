7e6eac105aed937f77861fe0f70c22831246329a
AleixAznarENTI <aleix.aznar@enti.cat>
2024-11-30 00:19:57 +0100

    Boss2

--- Assets/Scripts/Enemies/Boss2/Boss2.cs
+++ Assets/Scripts/Enemies/Boss2/Boss2.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Boss2 : Enemy
 {
+    public int speed;
+    public float minX;
+    public float maxX;
+    private bool firstPhase = true;
+    private bool secondPhase = true;
     private Animator animator;
     private float timer;
     private float lastAttack = 0f;
+    private bool attackCharged = false;
 
     protected override void Start()
     {
@@ -30,40 +37,85 @@ public class Boss2 : Enemy
 
     void ChooseAttack()
     {
-        float chosenAttack = Mathf.Floor(Random.Range(0, 3));
-        switch (chosenAttack)
-        {
-            case 0:
+        //float chosenAttack = Mathf.Floor(Random.Range(0, 3));
+        //switch (chosenAttack)
+        //{
+        //    case 0:
                 Attack1();
-                break;
-            case 1:
-                Attack2();
-                break;
-            case 2:
-                Attack3();
-                break;
-            default:
-                break;
-        }
+        //        break;
+        //    case 1:
+        //        Attack2();
+        //        break;
+        //    case 2:
+        //        Attack3();
+        //        break;
+        //    default:
+        //        break;
+        //}
     }
 
     void Attack1()
     {
+        //coletazo
 
     }
 
     void Attack2()
     {
-
+        //mucho pincho
     }
 
     void Attack3()
     {
+        //persecucion
+        if (!attackCharged)
+        {
+            StartCoroutine(Persecution(2));
+        }
+        
+        if (attackCharged)
+        {
+            if (transform.position.x > minX && firstPhase)
+            {
+                transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+            }
+            else if(transform.position.x < maxX && secondPhase)
+            {
+                firstPhase = false;
+                transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            }
+            else
+            {
+                secondPhase = false;
+                StartCoroutine(Persecution(2));
+            }
 
+        }
     }
 
     void ResetAttackTime(float waitTime)
     {
         lastAttack = Time.time + waitTime;
+    }
+
+    IEnumerator Persecution(float waitTime)
+    {
+        //animación cargar rotación
+        yield return new WaitForSeconds(waitTime*0.5f);
+        if (!attackCharged)
+        {
+            attackCharged = true;
+            //animación moverse
+
+        }
+        else
+        {
+            secondPhase = true;
+            firstPhase = true;
+            attackCharged = false;
+            lastAttack = waitTime;
+        }
+
+
     }
 }
